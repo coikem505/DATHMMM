7dfe9e5817c5df7ae3aa56cc97c9b409e7ed3f82
AnhVu280799 <coikem501@gmail.com>
2021-12-27 00:30:21 +0700

    update chức năng thêm giỏ hàng

--- them_vao_gio_hang.php
+++ them_vao_gio_hang.php
@@ -1,12 +1,12 @@
 <?php
 include "controllers/c_gio_hang.php";
 
-// $masp = $_POST['masp'];
-// $dongia = $_POST['dongia'];
-// $soluong = $_POST['soluong']*1;
+$masp = $_POST['masp'];
+$dongia = $_POST['dongia'];
+$soluong = $_POST['soluong']*1;
 
-// $c_gio_hang = new C_gio_hang();
-// $c_gio_hang->themGioHang($masp,$soluong,$dongia);
+$c_gio_hang = new C_gio_hang();
+$c_gio_hang->themGioHang($masp,$soluong,$dongia);
 header("location:thong_tin_gio_hang.php");
 
 ?>
