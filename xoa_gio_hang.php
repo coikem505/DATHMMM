4e0250dd225fcffef2ba4a1d851d1fde97241871
AnhVu280799 <coikem501@gmail.com>
2021-12-27 00:31:11 +0700

    update chức năng xoa giỏ hàng

--- xoa_gio_hang.php
+++ xoa_gio_hang.php
@@ -2,8 +2,7 @@
 
 include('controllers/c_gio_hang.php');
 include('models/m_san_pham.php');
-// $c_gio_hang=new C_gio_hang();
-// $gio_hang=$c_gio_hang->xoaGioHang();
+
     
     header('location:thong_tin_gio_hang.php');
 
